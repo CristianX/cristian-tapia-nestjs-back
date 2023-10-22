5099d33f0c96d57f70d4df2a327b1f4b8519bd51
CristianX <thecristianx@hotmail.com>
2023-10-22 17:10:28 -0500

    Añadiendo conexión con bdd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as fs from 'fs';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          type: 'postgres',
+          url: process.env.DATABASE_URL,
+          entities: [],
+          synchronize: true, //Desactivar en prod
+          // ssl: true,
+          extra: {
+            ssl: { rejectUnauthorized: false },
+            ca: fs.readFileSync(process.env.CA_CERT_PATH).toString(),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
